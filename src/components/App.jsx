dae5bcfed0dbaa3b477f68996586fa021aa96b7f
Viacheslav Kostenko <kostenko.uvd@gmail.com>
2024-01-19 14:00:06 +0200

    added lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,35 @@
-import Home from 'pages/Home/Home';
-import Movies from 'pages/Movies/Movies';
+import React, { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Layout from './Layout';
-import NotFound from 'pages/NotFound/NotFound';
-import MovieDetails from 'pages/MovieDetails/MovieDetails';
-import Cast from './NestedRoutes/Cast/Cast';
-import Reviews from './NestedRoutes/Reviews/Reviews';
+const Home = lazy(() => import('pages/Home/Home'));
+const Movies = lazy(() => import('pages/Movies/Movies'));
+const Layout = lazy(() => import('./Layout'));
+const NotFound = lazy(() => import('pages/NotFound/NotFound'));
+const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('./NestedRoutes/Cast/Cast'));
+const Reviews = lazy(() => import('./NestedRoutes/Reviews/Reviews'));
 
-export const App = () => {
+const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
 
-          <Route path="movies" element={<Movies />} />
+            <Route path="movies" element={<Movies />} />
 
-          <Route path="movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
+            <Route path="movies/:movieId" element={<MovieDetails />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
           </Route>
-        </Route>
 
-        <Route path="/some" element={<h2>Hello</h2>}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
+          <Route path="/some" element={<h2>Hello</h2>}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Suspense>
     </>
   );
 };
+
+export default App;
